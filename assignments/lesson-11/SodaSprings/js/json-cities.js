ba95f2ec3df92f6b8ecaf6fcd06a85c9f1484184
GMOAraujo <ara18010@byui.edu>
2019-07-05 16:23:48 -0600

    update

---
+++ assignments/lesson-11/SodaSprings/js/json-cities.js
@@ -0,0 +1,57 @@
+var townindex, towns;
+var section = document.querySelector('section.json-cities');
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function () {
+    var townInfo = request.response;
+    showTownsdata(townInfo);
+}
+
+    function showTownsdata(jsonObj) {
+        towns = jsonObj['towns'];
+
+        for (townindex = 0; townindex < towns.length; townindex++) {
+
+            if (towns[townindex].name == 'Soda Springs')
+                populateCityData(towns[townindex])
+        }
+
+
+
+    }
+
+    function populateCityData(jsonObj) {
+
+        var city = jsonObj;
+
+        var myUl = document.createElement('ul');
+        var myList1 = document.createElement('li');
+        var myList2 = document.createElement('li');
+        var myList3 = document.createElement('li');
+        
+
+        myList1.textContent = city.events[0];
+        myList2.textContent = city.events[1];
+        myList3.textContent = city.events[2]
+        
+        
+      
+       
+
+
+
+
+        myUl.appendChild(myList1);
+        myUl.appendChild(myList2);
+        myUl.appendChild(myList3);
+    
+       
+       
+        section.appendChild(myUl);
+    }
+
+  
