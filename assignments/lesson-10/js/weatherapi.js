e90e980de193390062149e6c155dabe6162645bf
GMOAraujo <ara18010@byui.edu>
2019-07-08 23:47:53 -0600

    update

--- assignments/lesson-10/js/weatherapi.js
+++ assignments/lesson-10/js/weatherapi.js
@@ -24,52 +24,77 @@ weatherRequest.onload =  function () {
 
     }
 
+    //Five days Forecast for Preston.
+
     let fivedayRequest = new XMLHttpRequest();
     let fivedayURLstring = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=b98e3357e7b0e7fd3abe8eb2da2f7ea8';
     fivedayRequest.open('Get',fivedayURLstring, true);
     fivedayRequest.send();
-    
-    fivedayRequest.onload =  function () {
-    
+
+
+
+    fivedayRequest.onload = function() {
         let fiveday = JSON.parse(fivedayRequest.responseText);
-        console.log(fiveday);
-               
+        // Populate the 5 day forecast
+        fivedayDisplay(fiveday);
     
-             for  (i = 0; i < 5; ++i) {
-           // document.getElementById('WedDay').innerHTML = fiveday.list[4].dt_txt;
-            time = fiveday.list[i].dt_txt;
-           // if (time.includes('18:00:00')) {
-
-                var date = new Date (fiveday.list[i].dt_txt * 1000);
-                var days = ["Sunday", "Monday", "Tuesday","Wednesday","Thursday", "Saturday" ];
-                var months = ["January", "February", "March", "April", "May", "June","July", "August","September", "October", "November", "December"];
-                var findDate = days[i] + "<br>" + months[i] + " " + date;
-
-                
-                document.getElementById('WedDay').innerHTML = fiveday.list[4].dt_txt;
-
-               
-
-                var fiveTemp= fivedayRequest.list[i].main.temp;
-                fiveTemp= Math.round(fiveTemp);
-                fiveDaystemp.push(fiveTemp);
-
-                var iconcode = fivedayRequest.list[i].weather[4].icon;
-                var icon_path = "https://openweathermap.org/img/wn/" + iconcode + ".png";
-                fiveDaysicons.push(icon_path);
-
-                //Days
-                var day = days[0];
-                document.getElementById('WedDay').setAttribute('src',  weatherData.list.length);
-            
-           // }
-            
-          
-        }
-        
-
     }
 
+        //Forecast Display Function
+    function fivedayDisplay(fiveday) {
+        console.log(fiveday);
+
+
+         // go through the list of forecast values looking for 18:00 (6:00 p.m.) records and record index
+    let daytemp = [];
+    let month = [];
+    let date = [];
+    let icon = [];
+    let day = 0;
+    fiveday.list.forEach(x => {
+        if (x.dt_txt.includes('18:00:00')) {
+            daytemp[day] = Math.round(x.main.temp);
+            month[day] = x.dt_txt.substring(5,7);
+            date[day] = x.dt_txt.substring(8, 10);
+            icon[day] = x.weather[0].icon;
+            console.log(day, daytemp[day], month[day], date[day], icon[day]);
+            day++;
+
+        }
+    });
+
+    var Months = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."];
+
+    // Populate the Header for the Forecast Table
+    document.getElementById('MonDay').innerHTML = Months[month[0] - 1] + " " + date[0];
+    document.getElementById('TueDay').innerHTML = Months[month[0] - 1] + " " + date[1];
+    document.getElementById('WedDay').innerHTML = Months[month[0] - 1] + " " + date[2];
+    document.getElementById('ThuDay').innerHTML = Months[month[0] - 1] + " " + date[3];
+    document.getElementById('FriDay').innerHTML = Months[month[0] - 1] + " " + date[4];
+
+    // Populate the weather forecast icon
+    var basepath = "http://openweathermap.org/img/wn/";
+
+    document.getElementById('weatherIcon1').src = basepath + icon[0] + ".png";
+    document.getElementById('weatherIcon2').src = basepath + icon[1] + ".png";
+    document.getElementById('weatherIcon3').src = basepath + icon[2] + ".png";
+    document.getElementById('weatherIcon4').src = basepath + icon[3] + ".png";
+    document.getElementById('weatherIcon5').src = basepath + icon[4] + ".png";
+
+    // Populate the high temperature forecast for the day
+    document.getElementById('MonTemp').innerHTML = daytemp[0];
+    document.getElementById('TueTemp').innerHTML = daytemp[1];
+    document.getElementById('WedTemp').innerHTML = daytemp[2];
+    document.getElementById('ThuTemp').innerHTML = daytemp[3];
+    document.getElementById('FriTemp').innerHTML = daytemp[4];
+} // End of Forecast Display //end of Forecast Display Function
+    
+
+
+
+
+
+    
 
 
     
